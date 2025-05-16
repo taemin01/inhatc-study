6ed13130ac740d9898c3a3cc02954d4472c12d30
taemin <jirr1010@gmail.com>
2025-05-16 15:19:31 +0900

    fix serverprogramming error

--- ServerProgramming/project/week5/src/main/java/org/example/week5/Week5Application.java
+++ ServerProgramming/project/week5/src/main/java/org/example/week5/Week5Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
 public class Week5Application {
 
     public static void main(String[] args) {
