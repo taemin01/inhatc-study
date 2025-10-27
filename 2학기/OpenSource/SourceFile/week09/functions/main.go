97887aa02ec04f3d73aa76c30059779aed29e2d4
taemin <jirr1010@gmail.com>
2025-10-27 11:27:36 +0900

    week09 pointer v2

--- 2학기/OpenSource/SourceFile/week09/functions/main.go
+++ 2학기/OpenSource/SourceFile/week09/functions/main.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 )
 
-func swap(first int, second int) {
+func swap(first *int, second *int) {
 	temp := 0
-	temp = first
-	first = second
-	second = temp
+	temp = *first
+	*first = *second
+	*second = temp
 }
 
 func main() {
 	var a, b int = 1, 5
 	fmt.Println(a, b)
-	swap(a, b)
+	swap(&a, &b) //주소 넘김
 	fmt.Println( a, b)
 }
