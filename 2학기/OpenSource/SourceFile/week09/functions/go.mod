08d90132f39189a807d278ac171008b27d0367d8
taemin <jirr1010@gmail.com>
2025-10-27 11:24:09 +0900

    pointer first commit

---
+++ 2학기/OpenSource/SourceFile/week09/functions/go.mod
@@ -0,0 +1,3 @@
+module paint
+
+go 1.25.1
---
+++ 2학기/OpenSource/SourceFile/week09/functions/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func swap(first int, second int) {
+	temp := 0
+	temp = first
+	first = second
+	second = temp
+}
+
+func main() {
+	var a, b int = 1, 5
+	fmt.Println(a, b)
+	swap(a, b)
+	fmt.Println( a, b)
+}
