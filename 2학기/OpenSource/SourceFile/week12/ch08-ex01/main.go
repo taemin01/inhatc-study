2f1dde7d0cd572962ab63dbcc3f62eb6498c3d1e
taemin <jirr1010@gmail.com>
2025-11-17 11:40:53 +0900

    ch08 v0.2

--- 2학기/OpenSource/SourceFile/week12/ch08-ex01/main.go
+++ 2학기/OpenSource/SourceFile/week12/ch08-ex01/main.go
@@ -9,12 +9,15 @@ type subscriber struct {
 
 func applyPrice(s *subscriber) { //여기서 그 주소에 대한 price 필드를 10000으로 할당
 	s.price = 10000
+	s.name = "Inha"
 }
 
 func main() {
 	var s1 subscriber
-	s1.name = "Inha"
+	var p *subscriber = &s1
 	applyPrice(&s1) //sub s1의 주소 인자로 전송
 
-	fmt.Println("name : ", s1.name, " / price : ", s1.price)
+	fmt.Println("name : ", s1.name, " / price : ", s1.price)     //구조체로 직접 접근
+	fmt.Println("name : ", p.name, " / price : ", p.price)       //포인터로 접근
+	fmt.Println("name : ", (*p).name, " / price : ", (*p).price) //포인터로 접근
 }
