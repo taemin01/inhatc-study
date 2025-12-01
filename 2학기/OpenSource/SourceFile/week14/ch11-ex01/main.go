898580f3560352131e1c399f117d62bad0454fe6
taemin <jirr1010@gmail.com>
2025-12-01 10:29:09 +0900

    week14 interface v0.1

---
+++ 2학기/OpenSource/SourceFile/week14/ch11-ex01/main.go
@@ -0,0 +1,94 @@
+package main
+
+import "fmt"
+
+// Pokemon 인터페이스 정의
+type Pokemon interface { //규격? 이 4가지의 함수를 구현하면 Pokemon 타입이 되는 것
+	//밑 포켓몬 타입을 파라미터로 받는 것을 Charmander로 하면 다른 구조체들은 타입이 맞지 않아 에러가 남
+	//이 4가지를 구현하지 않는다면 Pokemon 타입이 될 수 없음
+	Name() string
+	Type() string
+	Attack() int
+	Defense() int
+}
+
+// Charmander 구조체
+type Charmander struct { //배열은 같은 타입, 구조체는 필드의 집합체(다양한 자료형 가능)
+	hp int
+}
+
+func (c Charmander) Name() string { //c라는 리시버, 리시버가 있기 때문에 Name은 메서드 string은 리턴 타입 리시버 없으면 일반 함수
+	return "리자드"
+}
+
+func (c Charmander) Type() string {
+	return "불꽃"
+}
+
+func (c Charmander) Attack() int {
+	return 52
+}
+
+func (c Charmander) Defense() int {
+	return 43
+}
+
+// Squirtle 구조체
+type Squirtle struct {
+	hp int
+}
+
+func (s Squirtle) Name() string {
+	return "꼬부기"
+}
+
+func (s Squirtle) Type() string {
+	return "물"
+}
+
+func (s Squirtle) Attack() int {
+	return 48
+}
+
+func (s Squirtle) Defense() int {
+	return 65
+}
+
+// Bulbasaur 구조체
+type Bulbasaur struct {
+	hp int
+}
+
+func (b Bulbasaur) Name() string {
+	return "이상해씨"
+}
+
+func (b Bulbasaur) Type() string {
+	return "풀"
+}
+
+func (b Bulbasaur) Attack() int {
+	return 49
+}
+
+func (b Bulbasaur) Defense() int {
+	return 49
+}
+
+// Pokemon을 파라미터로 받는 함수
+func printPokemonInfo(p Pokemon) {
+	fmt.Printf("%s (%s)\n", p.Name(), p.Type())
+	fmt.Printf("공격: %d, 방어: %d\n\n",
+		p.Attack(), p.Defense())
+}
+
+func main() {
+	charmander := Charmander{hp: 39}
+	squirtle := Squirtle{hp: 44}
+	bulbasaur := Bulbasaur{hp: 45}
+
+	// 모두 Pokemon 인터페이스로 사용 가능
+	printPokemonInfo(charmander)
+	printPokemonInfo(squirtle)
+	printPokemonInfo(bulbasaur)
+}
