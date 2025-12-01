2d3e3785cbf883233405bec52ffdc29d45f434f5
taemin <jirr1010@gmail.com>
2025-12-01 11:35:49 +0900

    week14 panic v0.1

---
+++ 2학기/OpenSource/SourceFile/week14/ch11-ex02/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func safeDivide(a, b int) {
+	defer func() {
+		//err에 panic 메시지가 담기는 듯 defer로 뒤로 미뤄놨기에 safeDivide가
+		//끝나면서 err에 panic 메시지가 담겨오기에 출력해주고 recover로 복구하는 것
+		//예외처리임
+		if err := recover(); err != nil { //panic으로 끝나고 recover로 복구하기에 프로그램 계속 실행됨이 출력 되는 것임
+			fmt.Println("에러 발생:", err)
+		}
+	}()
+
+	if b == 0 {
+		panic("0으로 나눌 수 없습니다!")
+	}
+
+	result := a / b
+	fmt.Println("결과:", result)
+}
+
+func main() {
+	fmt.Println("첫 번째 호출")
+	safeDivide(10, 2)
+
+	fmt.Println("\n두 번째 호출")
+	safeDivide(10, 0)
+
+	fmt.Println("\n프로그램 계속 실행됨")
+}
--- 2학기/OpenSource/SourceFile/week14/ch12-ex01/main.go
+++ 2학기/OpenSource/SourceFile/week14/ch12-ex01/main.go
@@ -8,6 +8,7 @@ func main() {
 	//defer는 스택처럼 쌓이는 것
 	//Last In First Out
 	//파일 읽는 중 출력 후 마지막에 쌓인 3부터 2 1 순서로 감
+	//쓰다보면 요긴하게 쓰임 시험에 나오니까 알아두기
 	defer fmt.Println("1")
 	defer fmt.Println("2")
 	defer fmt.Println("3")
